2c9797925fde91b85308e483768e87445f28c1e4
GrandestJax <491659@student.roundrockisd.org>
2014-10-16 15:50:16 -0500

    It all done
    yay

--- lab9-100pt.py
+++ lab9-100pt.py
@@ -13,3 +13,32 @@
 # - A temperature of over 105F
 # - A temperature of over 102F and they have been sick in the last 24 hours
 # - A temperature over 100, OR they've been sick in the last 24 hours, AND they've recently travelled to West Africa.
+Patients = (23)
+
+while Patients >= 1:
+    print"---------------------"
+    print "what is your temperature?"
+    print"---------------------"
+    Temp_thing = int(raw_input())
+    if Temp_thing >= 105:
+        print"---------------------"    
+        print "you need to go see a doctor"
+        print"---------------------"    
+    print"---------------------"    
+    print "have you been sick in the past 24 hours?"
+    print"---------------------"    
+    Sick = raw_input()
+    if Sick == "yes" and Temp_thing >= 102:
+        print"---------------------"    
+        print "Go to the hospital"
+        print"---------------------"   
+    print"---------------------"    
+    print "have you been to africa in the past time?"
+    print"---------------------"    
+    Africa_chance = raw_input()
+    if Africa_chance == "yes" and Sick == "yes" and Temp_thing >= 100:
+        print"---------------------"    
+        print "GO TO THE EMERGENCY ROOM"
+        print"---------------------"
+    Patients = Patients - 1
+    print "NEXT!"
